c6fc669dcda8ea8f0ad7f1a68283aed3e347b4ed
DanielGCh1 <58349941+DanielGCh1@users.noreply.github.com>
2021-11-11 01:44:15 -0600

    cambios en todos los proyectos

--- BanamerServidor/src/banamerservidor/logica/Service.java
+++ BanamerServidor/src/banamerservidor/logica/Service.java
@@ -3,16 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package banamercliente.logica;
+package banamerservidor.logica;
 
 /**
  *
  * @author dgcha
  */
-
-
-// Este es el proxy 
-
 public class Service {
     
 }
