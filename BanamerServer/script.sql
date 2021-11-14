31729e18746488f11b61e41b14b94498a7a1c285
DanielGCh1 <58349941+DanielGCh1@users.noreply.github.com>
2021-11-13 22:25:33 -0600

    Update sql

--- BanamerServer/script.sql
+++ BanamerServer/script.sql
@@ -1,16 +1,32 @@
+-- MySQL Workbench Forward Engineering
 
-CREATE DATABASE Data;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-use Data;
+-- -----------------------------------------------------
+-- Schema Banamer
+-- -----------------------------------------------------
 
-create table Usuario (
-       usuario  varchar(10)  not null,
-       clave varchar(10) not null,  
-       Primary Key (usuario)         
-     );
+-- -----------------------------------------------------
+-- Schema Banamer
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Banamer` DEFAULT CHARACTER SET utf8 ;
+USE `Banamer` ;
 
-insert into Usuario values('001','001');
-insert into Usuario values('002','002');
-insert into Usuario values('003','003');
+-- -----------------------------------------------------
+-- Table `Banamer`.`Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Banamer`.`Usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(60) NOT NULL,
+  `saldo` INT NOT NULL,
+  `clave` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
 
--- source C:\jsanchez\UnaPrograIIIJava\Materiales\Sockets\Server-Client-Protocol\Server\script.sql
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
